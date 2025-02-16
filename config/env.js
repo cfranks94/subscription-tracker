23311fa43891595f3093af4352a02321a0cb74fd
Cord Franks <crfranks94@gmail.com>
2025-02-16 08:59:46 -0600

    implement user authentication with sign up, sign in, and sign out functionality; add user retrieval endpoints

--- config/env.js
+++ config/env.js
@@ -3,3 +3,6 @@ import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
+
+// Token -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2N2IxZjg5NGEyZjhiMDFlY2I3ZmFmZWYiLCJpYXQiOjE3Mzk3MTY3NTYsImV4cCI6MTczOTgwMzE1Nn0.glGxr7mh6TdAWJE1XbYP7vglreUCFkyGTjsS36XfdOE"
+// _ID -> "67b1f894a2f8b01ecb7fafef"
